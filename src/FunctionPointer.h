b6cfa401f66aa8129cae0c7f1e675359de1acdd3
Gerben <gerbenvaneerten@gmail.com>
2019-11-28 13:09:38 +0100

    Initial FunctionPointer header

---
+++ src/FunctionPointer.h
@@ -0,0 +1,37 @@
+#include <type_traits>
+#include <utility>
+
+using namespace std;
+
+template<typename _Tp>
+using decay_t = typename decay<_Tp>::type;
+
+template<typename Callable>
+union storage
+{
+    storage() {}
+    decay_t<Callable> callable;
+};
+
+template<int, typename Callable, typename Ret, typename... Args>
+auto FunctionPointer_(Callable&& c, Ret (*)(Args...))
+{
+    static bool used = false;
+    static storage<Callable> s;
+    using type = decltype(s.callable);
+
+    if(used)
+        s.callable.~type();
+    new (&s.callable) type(std::forward<Callable>(c));
+    used = true;
+
+    return [](Args... args) -> Ret {
+        return Ret(s.callable(std::forward<Args>(args)...));
+    };
+}
+
+template<typename Fn, int N = 0, typename Callable>
+Fn* FunctionPointer(Callable&& c)
+{
+    return FunctionPointer_<N>(std::forward<Callable>(c), (Fn*)nullptr);
+}
